1724c15e930acb6059878d94f04e9e73c1986323
Ash <acst52@gmail.com>
2023-03-08 15:35:47 -0500

    started working on dashboardRoutes, added corresponding code to allPostsAdmin.handlebars

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,7 +1,32 @@
 const router = require('express').Router();
-const { Post } = require('../models/');
+const { Post, User } = require('../models/');
 const withAuth = require('../utils/auth');
 
-// routes needed: 1. get route to get all the logged in user's posts ... post.findAll, include where property inside obj wherE userId: req.session.userId. Route same as the home route to get all posts for homepage. render into all posts admin using dashboard layout
+// get all auth user's posts
+router.get('/posts', withAuth, async (req, res) => {
+    try {
+        // now that user's been authenticated thanks to middleware specified above, let's get the current session's user id:
+        const userId = req.session.userId;
+
+        // let's find user id and incl all their posts
+        const user = await User.findByPk(userId, {
+            Include: [Post]
+        });
+
+        // Pass their posts to the view & render into all posts admin using dashboard layout:
+        const posts = user.posts;
+        res.render('allPostsAdmin', { posts });
+        
+    } catch (err) {
+        // res.status(500).send('You do not have any posts! To create a post, go to LINK')
+        console.log("This user does not have any active posts");
+        // if user has no active posts, redirect to post creation page:
+        res.redirect('/newPost');
+    }
+});
+
+// Routes still needed:
+
 // 2. get route with /new endpt, res.render for new post in the layout of dashboard
+
 // 3. get route with the /edit/:id endpoint .. post.findByPk(req.params.id), then render into edit post with layout of dashboard
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,16 +1,14 @@
 const router = require('express').Router();
 const { User } = require('../models');
-// const withAuth = require('../utils/auth');
 
-// get all posts for homepage:
+// get all posts for homepage
 router.get('/', async (req, res) => {
   try {
     const postData = await Post.findAll({
         include: [User]
     });
-
     const posts = postData.map((post) => post.get({ plain: true }));
-
+    // render on allPosts view
     res.render('allPosts', {
       posts
     });
@@ -19,7 +17,21 @@ router.get('/', async (req, res) => {
   }
 });
 
-// get single post by id /post/:id **
+// get single post by id /post/:id 
+router.get('/post/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [User]
+    });
+    const post = postData.get({ plain: true });
+    // render on singlePost view
+    res.render('singlePost', {
+      post
+    });
+  } catch (error) {
+    res.status(500).json(err);
+  }
+});
 
 // login route
 router.get('/login', (req, res) => {
@@ -27,10 +39,18 @@ router.get('/login', (req, res) => {
     res.redirect('/');
     return;
   }
-
+  // if logged in --> homepage, else --> login view
   res.render('login');
 });
 
-// signup route - same as login, except /signup res.render(signup)
+// signup route
+router.get('/signup', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+  // if logged in --> homepage, else --> signup view
+  res.render('signup');
+});
 
 module.exports = router;
