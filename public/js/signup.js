07fe1296699ceb1b468886fc3eec96dd722daf0b
Ash <acst52@gmail.com>
2023-04-25 21:43:12 -0400

    removed if statement from signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,7 +4,6 @@ const signupFormHandler = async (event) => {
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (username && password) {
       const response = await fetch('/api/user', {
         method: 'POST',
         body: JSON.stringify({ username, password }),
@@ -16,7 +15,6 @@ const signupFormHandler = async (event) => {
       } else {
         alert('Failed to sign up');
       }
-    }
   };
   
   document
