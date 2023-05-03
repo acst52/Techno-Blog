60d61984cb33118e1210ed863731fd412ed9b1e9
Ash <acst52@gmail.com>
2023-05-03 13:41:33 -0400

    fix event listener in edit.js, add event list to delete btn - now test

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,66 +1,23 @@
-// // add click event listener to all "Edit" buttons:
-
-// const editButtons = document.querySelectorAll('[data-bs-target="#editPostModal"]');
-// editButtons.forEach(button => {
-//   button.addEventListener('click', async (event) => {
-//     const postId = event.target.getAttribute('data-post-id');
-//     const response = await fetch(`/api/posts/${postId}`);
-//     if (response.ok) {
-//       const { id, title, content } = await response.json();
-//       document.querySelector('#edit-title-input').value = title;
-//       document.querySelector('#edit-content-input').value = content;
-//       document.querySelector('#id').setAttribute('data-post-id', id);
-//     }
-//   });
-// });
-
-// // Add a submit event listener to the "Edit Post" form
-// const editPostForm = document.querySelector('#editPostForm');
-// editPostForm.addEventListener('submit', async (event) => {
-//   event.preventDefault();
-//   const title = document.querySelector('#editPostTitle').value;
-//   const body = document.querySelector('#editPostBody').value;
-//   const postId = editPostForm.getAttribute('data-post-id');
-//   const response = await fetch(`/api/posts/${postId}`, {
-//     method: 'PUT',
-//     body: JSON.stringify({ title, body }),
-//     headers: {
-//       'Content-Type': 'application/json'
-//     }
-//   });
-//   if (response.ok) {
-//     location.reload();
-//   } else {
-//     alert('Failed to edit post');
-//   }
-// });
-
-// add click event listener to all "Edit" buttons:
-const editButtons = document.querySelectorAll(
-	'[data-bs-target="#editPostModal"]'
-);
-editButtons.forEach((button) => {
-	button.addEventListener('click', async (event) => {
-		const postId = event.target.getAttribute('data-post-id');
-		const response = await fetch(`/api/posts/${postId}`);
-		if (response.ok) {
-			const { id, title, content } = await response.json();
-			document.querySelector('input[name="post-title"]').value = title;
-			document.querySelector('textarea[name="post-body"]').value = content;
-			document
-				.querySelector('#edit-post-form')
-				.setAttribute('data-post-id', id);
-		}
-	});
+// add click event listener to edit btn
+const editButton = document.querySelector('#edit-btn');
+editButton.addEventListener('click', async (event) => {
+	const postId = event.target.getAttribute('data-post-id');
+	const response = await fetch(`/api/posts/${postId}`);
+	if (response.ok) {
+		const { id, title, content } = await response.json();
+		document.querySelector('input[name="post-title"]').value = title;
+		document.querySelector('textarea[name="post-body"]').value = content;
+		document.querySelector('#edit-post-form').setAttribute('data-post-id', id);
+	}
 });
 
-// Add a submit event listener to the "Edit Post" form
+// Add a submit event listener to edit post form
 const editPostForm = document.querySelector('#edit-post-form');
 editPostForm.addEventListener('submit', async (event) => {
 	event.preventDefault();
-	const title = document.querySelector('input[name="post-title"]').value;
-	const body = document.querySelector('textarea[name="post-body"]').value;
-	const postId = editPostForm.getAttribute('data-post-id');
+	const title = document.querySelector('#edit-post-title').value;
+	const body = document.querySelector('#edit-post-content').value;
+	const postId = editPostForm.getAttribute('#post-id');
 	const response = await fetch(`/api/posts/${postId}`, {
 		method: 'PUT',
 		body: JSON.stringify({ title, body }),
@@ -72,5 +29,24 @@ editPostForm.addEventListener('submit', async (event) => {
 		location.reload();
 	} else {
 		alert('Failed to edit post');
+	}
+});
+
+// add click event listener for delete btn
+const deleteButton = document.querySelector('#delete-btn');
+deleteButton.addEventListener('click', async (event) => {
+	event.preventDefault();
+	const postId = document.querySelector('#post-id').value;
+	const response = await fetch(`/api/posts/${postId}`, {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+	if (response.ok) {
+		// Redirect to the homepage if deleted successfully
+		location.assign('/');
+	} else {
+		alert('Failed to delete post');
 	}
 });
